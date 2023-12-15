53de8f6d3673b8a40203ab8c7d83b0a7238dadb2
Henrique Gravina <henrique@gravina.com.br>
2023-12-15 22:44:36 +0000

    Some changes to run on Vercel

--- IkePingDash.js
+++ IkePingDash.js
@@ -5,32 +5,28 @@
 
 */
 
-const { exec } = require('child_process');
-const fs = require('fs')
+const { exec } = require("child_process");
+const fs = require("fs");
 
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-var path = require('path');
-const express = require('express')
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+var path = require("path");
+const express = require("express");
 
-
-
-var path = require('path');
+var path = require("path");
 dir = path.join(__dirname);
 
 // Read hosts list from config/hosts.json file
-const hosts_list = JSON.parse(fs.readFileSync(dir + '/config/hosts.json'));
+const hosts_list = JSON.parse(fs.readFileSync(dir + "/config/hosts.json"));
 
 // Read groups definitions
-const groups_list = JSON.parse(fs.readFileSync(dir + '/config/groups.json'));
-
-
+const groups_list = JSON.parse(fs.readFileSync(dir + "/config/groups.json"));
 
 // When a client connects, send the host list
-io.on('connection', function(socket){
-  io.emit('groups_list', groups_list);
-  io.emit('hosts_list', hosts_list);
+io.on("connection", function (socket) {
+  io.emit("groups_list", groups_list);
+  io.emit("hosts_list", hosts_list);
 
   // Ping all hosts and send the results to clients
   pingAllHosts(hosts_list);
@@ -40,12 +36,12 @@ io.on('connection', function(socket){
 async function pingAllHosts(hosts) {
   for (const element of hosts) {
     const result = await pingHost(element.address);
-    const host_result = { "id": element.id, "host_status": result.result };
-    io.emit('result', host_result);
+    const host_result = { id: element.id, host_status: result.result };
+    io.emit("result", host_result);
   }
 }
 
-// Ping a host 
+// Ping a host
 function pingHost(host) {
   return new Promise((resolve, reject) => {
     exec(`ping -c 1 ${host}`, (error, stdout, stderr) => {
@@ -63,15 +59,15 @@ setInterval(() => {
   pingAllHosts(hosts_list);
 }, 30000); // 30 seconds
 
-// 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/html/dashboard.html');
+//
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/html/dashboard.html");
 });
 
-var dir = path.join(__dirname, '/html');
+var dir = path.join(__dirname, "/html");
 app.use(express.static(dir));
 
 // Start the server
-http.listen(3500, function() {
-  console.log('Listening on *:3500');
+http.listen(3500, function () {
+  console.log("Listening on *:3500");
 });
